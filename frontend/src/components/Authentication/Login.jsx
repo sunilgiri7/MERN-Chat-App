00640d75987f227aee51f4b955f46e436a6e472f
sunilgiri7 <seungiri841@gmail.com>
2024-03-25 22:32:21 -0700

    'errorsolve'

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
         },
       };
       const { data } = await axios.post(
-        "/api/user/login",
+        "https://your-render-app-name.onrender.com/api/user/login",
         { email, password },
         config
       );
